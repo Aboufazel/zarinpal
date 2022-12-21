4c862a9ec8ae13946174b700f08cb33b3c1b0cc6
Aboufazel <a.ab2521@gmail.com>
2022-12-22 02:00:16 +0330

    Ref : refactor container done

--- src/components/SecondaryBlock/SecondaryBlock.jsx
+++ src/components/SecondaryBlock/SecondaryBlock.jsx
@@ -23,7 +23,7 @@ const SecondaryBlock = () => {
     const handleClose = () => setShow(false);
     return (
         <div className={'w-100 h-100'} style={{backgroundColor: "white"}}>
-            <Container fluid dir={'rtl'}>
+            <Container dir={'rtl'}>
                 <Row className={'menu_Block'}>
                     <Offcanvas placement={"top"} show={show} onHide={handleClose}>
                         <Offcanvas.Header closeButton>
